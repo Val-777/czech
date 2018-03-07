9b9aedb8868e36e05a9d4e8fffbc43fd51cc423c
Val Melev <val.melev99@gmail.com>
2018-03-07 18:51:27 +0100

    Add 'home url resolves to home view' test

--- quiz/tests/tests.py
+++ quiz/tests/tests.py
@@ -1,9 +1,15 @@
-from django.urls import reverse
+from django.urls import reverse, resolve
 from django.test import TestCase
+
+from quiz.views import home
 
 
 class homeTests(TestCase):
-    def test_add_view_status_code(self):
+    def test_home_view_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 302)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
