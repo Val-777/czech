15682e590bdb99ff7f34604d44ea4fafaacf554b
Val Melev <val.melev99@gmail.com>
2018-03-25 22:34:04 +0200

    Add User account view

--- czech/urls.py
+++ czech/urls.py
@@ -39,4 +39,6 @@ urlpatterns = [
          name='password_change'),
     path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),
          name='password_change_done'),
+    path('settings/account/',
+         accounts_views.UserUpdateView.as_view(), name='my_account'),
 ]
