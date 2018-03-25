d4c07fb8a25275aa40eae55e3e066ae1334cbf90
Val Melev <val.melev99@gmail.com>
2018-03-25 17:29:34 +0200

    Add login required protection test

--- addwords/tests.py
+++ addwords/tests.py
@@ -5,14 +5,20 @@ from addwords.views import add
 
 
 class addTests(TestCase):
-    def test_add_view_status_code(self):
-        url = reverse('add')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
-
     def test_add_url_resolves_add_view(self):
         view = resolve('/add/')
         self.assertEquals(view.func, add)
+
+
+class LoginRequiredTests(TestCase):
+    def setUp(self):
+        self.url = reverse('add')
+        self.response = self.client.get(self.url)
+
+    def test_redirection(self):
+        login_url = reverse('login')
+        self.assertRedirects(self.response, '{login_url}?next={url}'.format(
+            login_url=login_url, url=self.url))
 
     # def test_add_Noun_view_status_code(self):
     #     url = reverse('add_word', kwargs={'kind': 'Noun'})
